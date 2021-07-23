bcf2e5618ca5e75b72d93bed3dc57277a3cee771
Mohammad-Dwairi <mdwairy@gmail.com>
2021-07-24 02:21:47 +0300

    Add validation on RegistrationRequest

--- src/main/java/com/mdwairy/momentsapi/registration/RegistrationRequest.java
+++ src/main/java/com/mdwairy/momentsapi/registration/RegistrationRequest.java
@@ -2,12 +2,27 @@ package com.mdwairy.momentsapi.registration;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class RegistrationRequest {
 
+    @NotNull
+    @Size(min = 2, max = 15, message = "First Name Should be between 2 and 15 characters")
     private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 15, message = "Last Name Should be between 2 and 15 characters")
     private String lastName;
+
+    @NotNull
+    @Email(message = "Please enter a valid email address")
     private String email;
+
+    @NotNull
+    @Size(min = 5, max = 30, message = "Password should be between 5 and 30 characters")
     private String password;
 
 }
