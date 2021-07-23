3a191a616c10f61ccc399dc31e3d4c9f2dd5621e
Mohammad-Dwairi <mdwairy@gmail.com>
2021-07-23 23:22:07 +0300

    Initial commit

---
+++ src/main/java/com/mdwairy/momentsapi/MomentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.mdwairy.momentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdwairy/momentsapi/MomentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdwairy.momentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
