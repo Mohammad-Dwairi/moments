7fd6a9107b8474635dbcbe7635961f6b2c5be486
Mohammad-Dwairi <mdwairy@gmail.com>
2021-07-24 02:30:46 +0300

    Enable requests to /registration/** without authentication

--- src/main/java/com/mdwairy/momentsapi/configurations/SecurityConfig.java
+++ src/main/java/com/mdwairy/momentsapi/configurations/SecurityConfig.java
@@ -16,6 +16,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/registration/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().formLogin();
     }
 }
