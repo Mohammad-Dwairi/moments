aa7c33286550ae12f12cdf347da37468289f65af
Mohammad-Dwairi <mdwairy@gmail.com>
2021-07-31 02:12:27 +0300

    Added CommentRepository Spring Data interface

---
+++ src/main/java/com/mdwairy/momentsapi/app/comment/CommentRepository.java
@@ -0,0 +1,6 @@
+package com.mdwairy.momentsapi.app.comment;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+}
