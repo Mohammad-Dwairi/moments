ae152c041804e3e638387648eda34a231a4eca83
Mohammad-Dwairi <mdwairy@gmail.com>
2021-07-30 16:09:39 +0300

    injected confirmationTokenService into EmailService.

--- src/main/java/com/mdwairy/momentsapi/email/EmailService.java
+++ src/main/java/com/mdwairy/momentsapi/email/EmailService.java
@@ -1,18 +1,18 @@
 package com.mdwairy.momentsapi.email;
 
+import com.mdwairy.momentsapi.registration.tokens.ConfirmationToken;
+import com.mdwairy.momentsapi.registration.tokens.ConfirmationTokenService;
+import com.mdwairy.momentsapi.users.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
-import javax.mail.SendFailedException;
 import javax.mail.internet.MimeMessage;
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -20,19 +20,19 @@ import java.util.concurrent.CompletableFuture;
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Value("${spring.mail.username}")
     private String senderEmail;
 
     @Async
-    public void send(String to, String email) throws SendFailedException {
-        try {
-            javaMailSender.send(buildMimeMessage(to, email));
-        }
-        catch (MailException | MessagingException e) {
-            log.error(e.getMessage());
-            throw new SendFailedException();
-        }
+    public void send(User user) throws MessagingException {
+        ConfirmationToken confirmationToken = confirmationTokenService.getNewConfirmationToken(user);
+        ConfirmationToken savedCT = confirmationTokenService.save(confirmationToken);
+        String link = "http://localhost:8080/registration/confirm?token="+savedCT.getToken();
+        String emailBody = EmailBuilder.buildEmail(user.getFirstName(), link);
+        MimeMessage mimeMessage = buildMimeMessage(user.getEmail(), emailBody);
+        javaMailSender.send(mimeMessage);
     }
 
     private MimeMessage buildMimeMessage(String to, String email) throws MessagingException {
