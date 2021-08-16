f59610674069cabb0c51e4c65a71c0cd55308158
Mohammad-Dwairi <mdwairy@gmail.com>
2021-08-16 13:05:09 +0300

    Added currentProfilePicture, currentCoverPicture members, and addProfilePicture method.

--- src/main/java/com/mdwairy/momentsapi/app/userdetails/AppUserDetails.java
+++ src/main/java/com/mdwairy/momentsapi/app/userdetails/AppUserDetails.java
@@ -1,12 +1,21 @@
 package com.mdwairy.momentsapi.app.userdetails;
 
 import com.mdwairy.momentsapi.app.model.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppUserDetails extends BaseEntity {
 
     @ElementCollection
@@ -21,9 +30,19 @@ public class AppUserDetails extends BaseEntity {
     @ElementCollection
     private List<String> works;
 
+    private String currentProfilePicture;
+    private String currentCoverPicture;
     private String livesIn;
     private String country;
     private String bio;
     private String joinedIn;
+
+
+    public void addProfilePicture(String pictureUrl) {
+        if (this.profilePictures == null) {
+            this.profilePictures = new ArrayList<>();
+        }
+        this.profilePictures.add(pictureUrl);
+    }
 
 }
