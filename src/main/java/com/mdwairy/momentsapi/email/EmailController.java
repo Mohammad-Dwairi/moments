92fe093ade15cd9d7eb74e20108b76e7c322a688
Mohammad-Dwairi <mdwairy@gmail.com>
2021-08-30 17:02:59 +0300

    added exception handling

--- src/main/java/com/mdwairy/momentsapi/email/EmailController.java
+++ src/main/java/com/mdwairy/momentsapi/email/EmailController.java
@@ -4,6 +4,7 @@ import com.mdwairy.momentsapi.users.User;
 import com.mdwairy.momentsapi.users.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
 
+import static com.mdwairy.momentsapi.constant.UserExceptionMessage.USER_NOT_FOUND_BY_EMAIL;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
@@ -24,12 +26,17 @@ public class EmailController {
 
     @GetMapping
     public String sendEmail(@RequestParam("email") String email) throws MessagingException {
-        User user = userService.getUserByEmail(email);
-        if (user != null) {
+        try {
+            User user = userService.findByEmail(email);
             emailService.send(user);
             return "Email Sent";
         }
-        throw new MessagingException("Failed to send the confirmation email");
+        catch (UsernameNotFoundException e) {
+            throw new UsernameNotFoundException(String.format(USER_NOT_FOUND_BY_EMAIL, email));
+        }
+        catch (MessagingException e) {
+            throw new MessagingException("Failed to send the confirmation email");
+        }
     }
 
 }
