8dd4d6e2e7915af3afdf619ba50dfb03a62a0774
Mohammad-Dwairi <mdwairy@gmail.com>
2021-08-30 16:48:06 +0300

    removed UserDetails implementation from User class.

--- src/main/java/com/mdwairy/momentsapi/users/User.java
+++ src/main/java/com/mdwairy/momentsapi/users/User.java
@@ -5,24 +5,20 @@ import com.mdwairy.momentsapi.app.userdetails.AppUserDetails;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Collection;
-import java.util.Collections;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-public class User implements UserDetails {
+public class User implements Serializable {
 
     @Id
     @JsonIgnore
@@ -49,10 +45,10 @@ public class User implements UserDetails {
     private String password;
 
     @JsonIgnore
-    private Boolean locked = false;
+    private Boolean isAccountLocked = false;
 
     @JsonIgnore
-    private Boolean enabled = false;
+    private Boolean isAccountEnabled = false;
 
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
@@ -67,39 +63,4 @@ public class User implements UserDetails {
         this.password = password;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRole.name());
-        return Collections.singletonList(authority);
-    }
-
-    @Override
-    @JsonIgnore
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    @JsonIgnore
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    @JsonIgnore
-    public boolean isAccountNonLocked() {
-        return !locked;
-    }
-
-    @Override
-    @JsonIgnore
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    @JsonIgnore
-    public boolean isEnabled() {
-        return enabled;
-    }
 }
