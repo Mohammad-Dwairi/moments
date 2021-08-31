5bf0418b2998651152bfd92f92f5e9ef9caf704b
Mohammad-Dwairi <mdwairy@gmail.com>
2021-08-31 18:57:32 +0300

    Refactored to use AuthenticationSuccessHandler and to handle login requests in json format

--- src/main/java/com/mdwairy/momentsapi/filter/AppAuthenticationFilter.java
+++ src/main/java/com/mdwairy/momentsapi/filter/AppAuthenticationFilter.java
@@ -1,16 +1,16 @@
 package com.mdwairy.momentsapi.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mdwairy.momentsapi.jwt.JWTResponse;
-import com.mdwairy.momentsapi.jwt.JWTService;
-import com.mdwairy.momentsapi.users.UserPrincipal;
+import com.mdwairy.momentsapi.dto.LoginRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -19,40 +19,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 @Slf4j
 @RequiredArgsConstructor
 public class AppAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
     private final AuthenticationFailureHandler authenticationFailureHandler;
-    private final JWTService jwtService;
 
     @Override
+    @SneakyThrows
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        Authentication authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        log.info("ATTEMPT AUTHENTICATION");
+
+        LoginRequestDto loginRequest = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
+        super.setDetails(request, authenticationToken);
         return authenticationManager.authenticate(authenticationToken);
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-                                            Authentication authResult) throws IOException {
-        UserPrincipal userPrincipal = (UserPrincipal) authResult.getPrincipal();
-        String accessToken = jwtService.getAccessToken(userPrincipal);
-        String refreshToken = jwtService.getRefreshToken(userPrincipal);
-        JWTResponse jwtResponse = jwtService.buildJWTResponse(userPrincipal.getUsername(), accessToken, refreshToken);
-        response.setContentType(APPLICATION_JSON_VALUE);
-        new ObjectMapper().writeValue(response.getOutputStream(), jwtResponse);
+                                            Authentication authResult) throws IOException, ServletException {
+        log.info("SUCCESSFUL AUTHENTICATION");
+        authenticationSuccessHandler.onAuthenticationSuccess(request, response, authResult);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                               AuthenticationException failed) throws IOException, ServletException {
+        log.info("UNSUCCESSFUL AUTHENTICATION");
         authenticationFailureHandler.onAuthenticationFailure(request, response, failed);
-        //super.unsuccessfulAuthentication(request, response, failed);
     }
 }
