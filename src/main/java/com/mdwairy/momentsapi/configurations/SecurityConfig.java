41a170fd87b5e094944677bf8be2013a89fa96b4
Mohammad-Dwairi <mdwairy@gmail.com>
2021-08-31 19:02:07 +0300

    Added the custom AppAuthenticationEntryPoint and authenticationSuccessfulHandler to the configuration.

--- src/main/java/com/mdwairy/momentsapi/configurations/SecurityConfig.java
+++ src/main/java/com/mdwairy/momentsapi/configurations/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.mdwairy.momentsapi.configurations;
 
 import com.mdwairy.momentsapi.filter.AppAuthenticationFilter;
 import com.mdwairy.momentsapi.filter.AppAuthorizationFilter;
+import com.mdwairy.momentsapi.filter.AppAuthenticationEntryPoint;
 import com.mdwairy.momentsapi.handler.AppAuthenticationFailureHandler;
+import com.mdwairy.momentsapi.handler.AppAuthenticationSuccessHandler;
 import com.mdwairy.momentsapi.jwt.JWTService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +17,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -30,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final JWTService jwtService;
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
@@ -38,12 +41,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/registration/**", "/token/**", "/email", "/users/**", "/h2-console/**").permitAll();
         http.headers().frameOptions().disable();
+        http.authorizeRequests().antMatchers("/registration/**", "/token/**", "/email", "/users/**", "/h2-console/**").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
-        http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(UNAUTHORIZED));
-        http.addFilter(new AppAuthenticationFilter(authenticationManagerBean(), authenticationFailureHandler(), jwtService));
-        http.addFilterBefore(new AppAuthorizationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
+        http.addFilter(getAuthenticationFilter());
+        http.addFilterBefore(getAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.sessionManagement().sessionCreationPolicy(STATELESS);
     }
 
@@ -61,6 +64,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationFailureHandler authenticationFailureHandler() {
         return new AppAuthenticationFailureHandler();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new AppAuthenticationSuccessHandler(jwtService);
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return new AppAuthenticationEntryPoint();
+    }
+
+    private AppAuthenticationFilter getAuthenticationFilter() throws Exception {
+        return new AppAuthenticationFilter(authenticationManagerBean(), authenticationSuccessHandler(), authenticationFailureHandler());
+    }
+
+    private AppAuthorizationFilter getAuthorizationFilter() {
+        return new AppAuthorizationFilter(jwtService);
     }
 
 }
