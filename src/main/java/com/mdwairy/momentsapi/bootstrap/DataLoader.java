8c150b3eaaec26b267dc039689ad4447c40ff15a
Mohammad-Dwairi <mdwairy@gmail.com>
2021-08-31 18:41:18 +0300

    pre-load data to use in development profile

---
+++ src/main/java/com/mdwairy/momentsapi/bootstrap/DataLoader.java
@@ -0,0 +1,29 @@
+package com.mdwairy.momentsapi.bootstrap;
+
+import com.mdwairy.momentsapi.registration.RegistrationRequest;
+import com.mdwairy.momentsapi.registration.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Profile("dev")
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final RegistrationService registrationService;
+
+    @Override
+    public void run(String... args) {
+        RegistrationRequest request = new RegistrationRequest();
+        request.setFirstName("Mohammad");
+        request.setLastName("Dwairi");
+        request.setEmail("mdwairy@gmail.com");
+        request.setPassword("12345");
+        registrationService.register(request);
+        log.info("User Registered... ");
+    }
+}
